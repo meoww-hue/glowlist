345c75bb6a6dc16b0bf6336a0008d985098d971b
Meowmiaww <rengganisarin@gmail.com>
2026-08-11 18:18:58 +0700

    membuat app.jsx, sidebar.jsx, tentang.jsx

---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,3 @@
+export default function Tentang() {
+    return <h2>Haii namaku rengganis larasafatma arini dari kelas XII RPL 2, alasanku suka skincare karena aku berusaha untuk merawat diriku sendiri</h2>;
+}
